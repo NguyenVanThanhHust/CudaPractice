94f7734bcd45edd35f23d51eee3775b0e43deab5
NguyenVanThanhHust <nguyen.van.thanh.hust@gmail.com>
2023-02-15 16:32:19 +0700

    matrix multiplication

---
+++ programming_massively_parallel_processors/Chapter 3/simpleMatrixMultiplication.cu
@@ -0,0 +1,40 @@
+#include <cuda_runtime.h>
+#include <string>
+#include <iostream>
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define MATRIX_HEIGHT 100
+#define MATRIX_WIDTH 200
+
+int main()
+{
+    // Host 2d array
+    float h_A[MATRIX_WIDTH][MATRIX_HEIGHT];
+    float h_B[MATRIX_WIDTH][MATRIX_HEIGHT];
+    float h_B[MATRIX_WIDTH][MATRIX_HEIGHT];
+
+    // Check if array is created
+    if (h_A==NULL || h_B==NULL || h_C==NULL)
+    {
+        cout<<"Can't create host array"<<endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Initialize host vector
+    for (int  row_index = 0; row_index < MATRIX_HEIGHT; row_index++)
+    {
+        for (int col_index = 0; col_index < MATRIX_WIDTH; col_index++)
+        {
+            h_A[row_index][col_index] = float(row_index * MATRIX_WIDTH + 1);
+            h_B[row_index][col_index] = float(row_index * MATRIX_WIDTH + 2);
+        }
+        
+    }
+    
+    
+
+}
