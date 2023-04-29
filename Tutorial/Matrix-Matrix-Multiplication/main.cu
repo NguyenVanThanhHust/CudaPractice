2b7a7706ca857c4b71851c5644faef92d0edf5e0
NguyenVanThanhHust@gmail.com <nguyen.van.thanh.hust@gmail.com>
2023-04-29 23:19:23 +0700

    mat mul

--- Tutorial/Matrix-Matrix-Multiplication/main.cu
+++ Tutorial/Matrix-Matrix-Multiplication/main.cu
@@ -9,6 +9,10 @@
 
 #define assertm(exp, msg) assert(((void)msg, exp))
 
+using std::cout;
+using std::endl;
+
+
 __global__ void matMulKernel(float *d_A, float *d_B, float *d_C, int height_A, int width_A, int height_B, int width_B)
 {
     int row_threadIdx = blockDim.x * blockIdx.x + threadIdx.x;
@@ -68,7 +72,7 @@ int main(int argc, char **argv)
 
     // Declare device array
     float *d_A=NULL, *d_B=NULL, *d_C=NULL;
-    int sizeA = height_A * width_A;
+    int sizeA = height_A * width_A * sizeof(float);
     err = cudaMalloc((void **)&d_A, sizeA);
     if (err != cudaSuccess)
     {
@@ -77,19 +81,19 @@ int main(int argc, char **argv)
     }
 
 
-    int sizeB = height_B * width_B;
+    int sizeB = height_B * width_B * sizeof(float);
     err = cudaMalloc((void **)&d_B, sizeB);
     if (err != cudaSuccess)
     {
-        std::cout<<"Failed to allocate device vector for vector A  "<<cudaGetErrorString(err)<<" at file: "<<__FILE__<<" at line: "<<__LINE__<<std::endl;
+        std::cout<<"Failed to allocate device vector for vector B  "<<cudaGetErrorString(err)<<" at file: "<<__FILE__<<" at line: "<<__LINE__<<std::endl;
         exit(EXIT_FAILURE);
     }
 
-    int sizeC = height_A * width_B;
+    int sizeC = height_A * width_B * sizeof(float);
     err = cudaMalloc((void **)&d_C, sizeC);
     if (err != cudaSuccess)
     {
-        std::cout<<"Failed to allocate device vector for vector A  "<<cudaGetErrorString(err)<<" at file: "<<__FILE__<<" at line: "<<__LINE__<<std::endl;
+        std::cout<<"Failed to allocate device vector for vector C  "<<cudaGetErrorString(err)<<" at file: "<<__FILE__<<" at line: "<<__LINE__<<std::endl;
         exit(EXIT_FAILURE);
     }
 
@@ -100,14 +104,76 @@ int main(int argc, char **argv)
         std::cout<<std::endl;
     }
 
-    err = cudaMemcpy(h_A, d_A, size, cudaMemcpyHostToDevice);
+    err = cudaMemcpy(d_A, h_A, sizeA, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess)
+    {
+        cout<<"Failed to copy from host vector A to device vector A "<<cudaGetErrorString(err)<<" at file: "<<__FILE__<<" at line: "<<__LINE__<<endl;
+        exit(EXIT_FAILURE);
+    }
 
+    err = cudaMemcpy(d_B, h_B, sizeB, cudaMemcpyHostToDevice);
+    if (err != cudaSuccess)
+    {
+        cout<<"Failed to copy from host vector B to device vector B "<<cudaGetErrorString(err)<<" at file: "<<__FILE__<<" at line: "<<__LINE__<<endl;
+        exit(EXIT_FAILURE);
+    }
 
     dim3 threadsPerBlock(16, 16, 1);
-    dim3 blocksPerGrid(1, 1, 1;);
+    dim3 blocksPerGrid(1, 1, 1);
     matMulKernel<<<threadsPerBlock, blocksPerGrid>>>(d_A, d_B, d_C, height_A, width_A, height_B, width_B);   
     cudaDeviceSynchronize();
-    cudaError_t result = cudaMemcpy(dest, start_, min * sizeof(T), cudaMemcpyDeviceToHost);
+    cudaError_t result = cudaMemcpy(h_C, d_C, sizeC, cudaMemcpyDeviceToHost);
+
+    if (err != cudaSuccess)
+    {
+        std::cout<<"Failed to copy from device to host vector C  "<<cudaGetErrorString(err)<<" at file: "<<__FILE__<<" at line: "<<__LINE__<<std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    for(int row_index=0; row_index < height_A; row_index++){
+        for(int col_index=0; col_index < width_B; col_index++){
+            cout<<h_C[row_index * width_B + col_index]<< " ";
+        }
+        std::cout<<std::endl;
+    }
+
+    for(int row_index=0; row_index < height_A; row_index++){
+        for(int col_index=0; col_index < width_B; col_index++){
+            if (fabs(cpu_C[row_index * width_B + col_index] - h_C[row_index * width_B + col_index]) > 1e-5){
+                cout<<"Failed at row "<<row_index<<" col: "<<col_index;
+                cout<<" expect: "<<cpu_C[row_index * width_B + col_index]<<" get: "<<h_C[row_index * width_B + col_index]<<endl;
+                exit(EXIT_FAILURE);
+            }
+        }
+        std::cout<<std::endl;
+    }
+    cout<<"Test passed"<<endl;
+
+    err = cudaFree(d_A);
+    if (err != cudaSuccess)
+    {
+        cout<<"Failed to free data on device A "<<cudaGetErrorString(err)<<" at file: "<<__FILE__<<" at line: "<<__LINE__<<endl;
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaFree(d_B);
+    if (err != cudaSuccess)
+    {
+        cout<<"Failed to free data on device B "<<cudaGetErrorString(err)<<" at file: "<<__FILE__<<" at line: "<<__LINE__<<endl;
+        exit(EXIT_FAILURE);
+    }
+
+    err = cudaFree(d_C);
+    if (err != cudaSuccess)
+    {
+        cout<<"Failed to free data on device C "<<cudaGetErrorString(err)<<" at file: "<<__FILE__<<" at line: "<<__LINE__<<endl;
+        exit(EXIT_FAILURE);
+    }
+
+    delete[] h_A;
+    delete[] h_B;
+    delete[] h_C;
+    delete[] cpu_C;
 
     return 0;
 }
